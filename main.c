f5e03cf79bd8623995687d6756f03ba3ffc36e67
Thomas Binetruy <tbinetruy@gmail.com>
2018-09-01 17:40:55 -0400

    genisis

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <editline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mpc.h"
+
+int main(int argc, char **argv) {
+
+  /* Print Version and Exit Information */
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  /* Create Some Parsers */
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                           \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+            Number, Operator, Expr, Lispy);
+
+  /* In a never ending loop */
+  while (1) {
+
+    /* Output our prompt */
+    char *input = readline("lispy> ");
+    add_history(input);
+
+    /* Attempt to Parse the user Input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On Success Print the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise Print the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    free(input);
+  }
+
+  /* Undefine and Delete our Parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
+  return 0;
+}
